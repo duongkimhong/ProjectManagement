4a57cea0d6db40f480e8b9a5c2886e9785535172
duongkimhong <kimhongduong2109@gmail.com>
2024-03-14 21:35:53 +0700

    branch b2

--- ProjectManagement/Areas/Admin/Controllers/IssuesController.cs
+++ ProjectManagement/Areas/Admin/Controllers/IssuesController.cs
@@ -10,6 +10,7 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.EntityFrameworkCore;
 using ProjectManagement.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ProjectManagement.Areas.Admin.Controllers
 {
@@ -92,13 +93,53 @@ namespace ProjectManagement.Areas.Admin.Controllers
 				return NotFound();
 			}
 
-			var issue = await _context.Issues.FindAsync(issueId);
+			var issue = await _context.Issues
+				.Include(i => i.Assignee)
+				.Include(i => i.Reporter) 
+				.Include(i => i.Epics)
+				//.Include(i => i.Sprints)
+				.FirstOrDefaultAsync(i => i.Id == issueId);
 			if (issue == null)
 			{
 				return NotFound();
 			}
+			// Kiểm tra và gán giá trị từ Assignee và Reporter
+			var assigneeFullName = issue.Assignee != null ? issue.Assignee.FullName : null;
+			var assigneeImage = issue.Assignee != null ? issue.Assignee.Image : null;
+			var reporterFullName = issue.Reporter != null ? issue.Reporter.FullName : null;
+			var reporterImage = issue.Reporter != null ? issue.Reporter.Image : null;
 
-			return Json(issue);
+			Guid? epicId = null;
+			var epic = await _context.Epics.FirstOrDefaultAsync(e => e.Id == issue.EpicID);
+			if (epic != null)
+			{
+				epicId = epic.Id;
+			}
+			return Json(new
+			{
+				Id = issue.Id,
+				Name = issue.Name,
+				Type = issue.Type,
+				StartDate = issue.StartDate,
+				EndDate = issue.EndDate,
+				Description = issue.Description,
+				Status = issue.Status,
+				Priority = issue.Priority,
+				IsFlag = issue.IsFlag,
+				StoryPoint = issue.StoryPoint,
+				SprintId = issue.SprintID,
+				EpicId = epicId,
+				Assignee = new
+				{
+					FullName = assigneeFullName,
+					Image = assigneeImage
+				},
+				Reporter = new
+				{
+					FullName = reporterFullName,
+					Image = reporterImage
+				}
+			});
 		}
 
 		// POST: Admin/Issues/Edit/5
@@ -138,16 +179,16 @@ namespace ProjectManagement.Areas.Admin.Controllers
 			return View(issues);
 		}
 
-		public async Task<IActionResult> updateIssueSprint(Guid issueId, Guid sId)
+		public async Task<IActionResult> updateIssueSprint(Guid issueId, Guid sprintId)
 		{
 			try
 			{
 				//Guid id = Guid.Parse(issueId);
 				var issue = await _context.Issues.FindAsync(issueId);
-				var sprint = await _context.Sprints.FindAsync(sId);
-				issue.SprintID = sId;
+				var sprint = await _context.Sprints.FindAsync(sprintId);
+				issue.SprintID = sprintId;
 				issue.Sprints = sprint;
-				var projectId = sprint.ProjectID;
+				//var projectId = sprint.ProjectID;
 				_context.Update(issue);
 				await _context.SaveChangesAsync();
 
@@ -157,7 +198,6 @@ namespace ProjectManagement.Areas.Admin.Controllers
 			}
 			catch (Exception ex)
 			{
-				// Trong trường hợp có lỗi xảy ra, bạn có thể trả về một phản hồi lỗi
 				return StatusCode(500, $"An error occurred: {ex.Message}");
 			}
 		}
@@ -179,33 +219,6 @@ namespace ProjectManagement.Areas.Admin.Controllers
 			else
 			{
 				return Json(new { success = false });
-			}
-		}
-
-		public async Task<IActionResult> UpdateIssueStatus(string issueId, string status)
-		{
-			try
-			{
-				Guid id = Guid.Parse(issueId);
-				var issue = await _context.Issues.FindAsync(id);
-				if(status == "Todo")
-				{
-					issue.Status = IssueStatus.Todo;
-				} else if(status == "InProgress")
-				{
-					issue.Status = IssueStatus.InProgress;
-				}
-				else
-				{
-					issue.Status = IssueStatus.Done;
-				}
-				_context.SaveChanges();
-				return Json(new { success = true });
-			}
-			catch (Exception ex)
-			{
-				// Trong trường hợp có lỗi xảy ra, bạn có thể trả về một phản hồi lỗi
-				return StatusCode(500, $"An error occurred: {ex.Message}");
 			}
 		}
 
@@ -254,7 +267,22 @@ namespace ProjectManagement.Areas.Admin.Controllers
 		}
 
 		[HttpPost]
-		[ValidateAntiForgeryToken]
+		// BUG: không lấy đúng id của issue cần unlink epic
+		public async Task<IActionResult> UnlinkEpic(Guid issueId)
+		{
+			var issue = _context.Issues.Find(issueId);
+			if (issue != null)
+			{
+				issue.EpicID = null;
+				issue.Epics = null;
+				_context.Update(issue);
+				await _context.SaveChangesAsync();
+				return Json(new { success = true });
+			}
+			return Json(new { success = false });
+		}
+
+		[HttpPost]
 		public async Task<IActionResult> UpdateIssueType(Guid issueId, string type)
 		{
 			var issue = await _context.Issues.FindAsync(issueId);
@@ -281,19 +309,200 @@ namespace ProjectManagement.Areas.Admin.Controllers
 			return Json(new { success = false });
 		}
 
-		[HttpPost]
-		public async Task<IActionResult> UnlinkEpic(Guid issueId)
+		public async Task<IActionResult> UpdateIssueStatus(string issueId, string status)
 		{
-			var issue = _context.Issues.Find(issueId);
-			if (issue != null)
+			try
 			{
-				issue.EpicID = null;
-				issue.Epics = null;
-				_context.Update(issue);
-				await _context.SaveChangesAsync();
+				Guid id = Guid.Parse(issueId);
+				var issue = await _context.Issues.FindAsync(id);
+				if (status == "Todo")
+				{
+					issue.Status = IssueStatus.Todo;
+				}
+				else if (status == "In Progress")
+				{
+					issue.Status = IssueStatus.InProgress;
+				}
+				else
+				{
+					issue.Status = IssueStatus.Done;
+				}
+				_context.SaveChanges();
 				return Json(new { success = true });
 			}
-			return Json(new { success = false });
+			catch (Exception ex)
+			{
+				// Trong trường hợp có lỗi xảy ra, bạn có thể trả về một phản hồi lỗi
+				return StatusCode(500, $"An error occurred: {ex.Message}");
+			}
+		}
+
+		public async Task<IActionResult> updateIssuePriority(string issueId, string priority)
+		{
+			try
+			{
+				Guid id = Guid.Parse(issueId);
+				var issue = await _context.Issues.FindAsync(id);
+				if (priority == "Lowest")
+				{
+					issue.Priority = Priorities.Lowest;
+				}
+				else if (priority == "Low")
+				{
+					issue.Priority = Priorities.Low;
+				} else if (priority == "Medium")
+				{
+					issue.Priority = Priorities.Medium;
+				} else if (priority == "High")
+				{
+					issue.Priority = Priorities.High;
+				}
+				else
+				{
+					issue.Priority = Priorities.Highest;
+				}
+				_context.SaveChanges();
+				return Json(new { success = true });
+			}
+			catch (Exception ex)
+			{
+				// Trong trường hợp có lỗi xảy ra, bạn có thể trả về một phản hồi lỗi
+				return StatusCode(500, $"An error occurred: {ex.Message}");
+			}
+		}
+
+		public async Task<IActionResult> UpdateIssueEpic(Guid issueId, Guid epicId)
+		{
+			try
+			{
+				var issue = await _context.Issues.FindAsync(issueId);
+				var epic = await _context.Epics.FindAsync(epicId);
+				if(issue != null && epic != null)
+				{
+					issue.EpicID = epicId;
+					issue.Epics = epic;
+				} else if(epic == null && issue != null)
+				{
+					issue.EpicID = null;
+					issue.Epics = null;
+				}
+				_context.SaveChanges();
+				return Json(new { success = true });
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, $"An error occurred: {ex.Message}");
+			}
+		}
+
+		public async Task<IActionResult> UpdateIssueName(Guid issueId, string name)
+		{
+			try
+			{
+				var issue = await _context.Issues.FindAsync(issueId);
+				if (issue != null)
+				{
+					issue.Name = name;
+				}
+				_context.SaveChanges();
+				return Json(new { success = true });
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, $"An error occurred: {ex.Message}");
+			}
+		}
+
+		public async Task<IActionResult> UpdateIssueDescription(Guid issueId, string description)
+		{
+			try
+			{
+				var issue = await _context.Issues.FindAsync(issueId);
+				if (issue != null)
+				{
+					issue.Description = description;
+				}
+				_context.SaveChanges();
+				return Json(new { success = true });
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, $"An error occurred: {ex.Message}");
+			}
+		}
+
+		public async Task<IActionResult> UpdateIssueStartDate(Guid issueId, DateTime date)
+		{
+			try
+			{
+				var issue = await _context.Issues.FindAsync(issueId);
+				if (issue != null)
+				{
+					issue.StartDate = date;
+				}
+				_context.SaveChanges();
+				return Json(new { success = true });
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, $"An error occurred: {ex.Message}");
+			}
+		}
+
+		public async Task<IActionResult> UpdateIssueEndDate(Guid issueId, DateTime date)
+		{
+			try
+			{
+				var issue = await _context.Issues.FindAsync(issueId);
+				if (issue != null)
+				{
+					issue.EndDate = date;
+				}
+				_context.SaveChanges();
+				return Json(new { success = true });
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, $"An error occurred: {ex.Message}");
+			}
+		}
+
+		public async Task<IActionResult> UpdateIssueStoryPoint(Guid issueId, int storyPoint)
+		{
+			try
+			{
+				var issue = await _context.Issues.FindAsync(issueId);
+				if (issue != null)
+				{
+					issue.StoryPoint = storyPoint;
+				}
+				_context.SaveChanges();
+				return Json(new { success = true });
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, $"An error occurred: {ex.Message}");
+			}
+		}
+
+		public async Task<IActionResult> UpdateIssueAssignee(Guid issueId, string userId)
+		{
+			try
+			{
+				var issue = await _context.Issues.FindAsync(issueId);
+				var user = await _context.Users.FindAsync(userId);
+				if (issue != null)
+				{
+					issue.AssigneeID = userId;
+					issue.Assignee = user;
+				}
+				_context.SaveChanges();
+				return Json(new { success = true });
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, $"An error occurred: {ex.Message}");
+			}
 		}
 	}
 }
